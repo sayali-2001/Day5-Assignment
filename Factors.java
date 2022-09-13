e6b6d84cf25076466dda01d1f7ebac29a9eaf682
sayali-2001 <sayalithakare47@gmail.com>
2022-09-13 20:14:26 +0530

    Factors of Number

---
+++ Factors.java
@@ -0,0 +1,17 @@
+package Example;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int num1 = sc.nextInt();
+        int fact= 1;
+        for(int i = num1 ; i>=1 ; i--){
+            fact = fact * i;
+        }
+        System.out.println("Factors " +fact);
+    }
+}
